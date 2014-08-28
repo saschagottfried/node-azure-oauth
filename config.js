30e2587781c872a518061a8453aa0bbcc6d58ba5
Sascha Gottfried <sascha.gottfried@googlemail.com>
2014-08-28 15:01:10 +0200

    added other application configuration

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 ﻿
 
 module.exports.settings = {
-    authUrl: 'https://login.windows.net/<your_directory_id>/oauth2/authorize',
-    tokenUrl: 'https://login.windows.net/<your_directory_id>/oauth2/token',
-    clientId: 'guid_for_configured_app',
-    clientSecret: 'secret_for_configured_app',
-    callbackUrl: 'url_for_your_app_called_by_azure_upon_completing_authentication',
+    authUrl: 'https://login.windows.net/fireframeworkers.onmicrosoft.com/oauth2/authorize',
+    tokenUrl: 'https://login.windows.net/fireframeworkers.onmicrosoft.com/oauth2/token',
+    clientId: '5a6af026-e183-4599-b2f1-193d66e79fe9',
+    clientSecret: 'nVUCGQ+6BJ5zDU2YdcASUOr0t7J3pDGbhVcnluLGvD8=',
+    callbackUrl: 'http://securingnodejs.azurewebsites.net/auth/waad/callback',
     resource: 'https://graph.windows.net',
     mongodb: 'mongodb://MongoLab-1l:Y3n.k50P9WSll3qxdqaZ3VHrUBppUjjn68LYrc2jJlM-@ds035787.mongolab.com:35787/MongoLab-1l'
 };
